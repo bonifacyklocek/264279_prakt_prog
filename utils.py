64a7e37d1ec03e64692fc77041114c469eac8df1
bonifacyklocek <136375326+bonifacyklocek@users.noreply.github.com>
2024-06-11 15:17:50 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ high level support for doing this and that.
 def add ( a : int , b : int ) -> int:
     """Nie wiem po co to, ale wywala blad"""
     return a + b
-def subtract ( a : int , b : int ) -> int
+def subtract ( a : int , b : int ) -> int:
     """Nie wiem po co to, ale wywala blad"""
     return a - b
 def multiply ( a : int , b : int ) -> int:
